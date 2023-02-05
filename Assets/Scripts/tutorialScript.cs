cb3ecce9a3d96b1c24b8e60ee690dd3b4dd720df
BBeelzebul <delfipelfi@gmail.com>
2023-02-05 11:21:39 -0300

    tutorial

---
+++ Assets/Scripts/tutorialScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialScript : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
